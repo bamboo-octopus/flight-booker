3086f0a957a295bb339f0182b5931b0bb23b9b98
bamboo_octopus <bamboooctopus@gmail.com>
2022-03-05 14:39:12 -0500

    associations between flights and airports complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Airport.create(code: "JIA", city_state: "Jacksonville, FL" )
-Airport.create(code: "MIA" , city_state: "Miami, FL")
-Airport.create(code: "ORD" , city_state: "Chicago, IL" )
-Airport.create(code: "DEN" , city_state: "Denver, CO" )
-Airport.create(code: "JFK" , city_state: "New York, New York" )
-Airport.create(code: "IAD" , city_state: "Washington, D.C.")
-Airport.create(code: "SFO" , city_state: "San Francisco, CA" )
-Airport.create(code: "AUS" , city_state: "Austin, TX")
-Airport.create(code: "OMA" , city_state: "Omaha, NE" )
-Airport.create(code: "SEA" , city_state: "Seattle, WA")
+# Airport.create(code: "JIA", city_state: "Jacksonville, FL" )
+# Airport.create(code: "MIA" , city_state: "Miami, FL")
+# Airport.create(code: "ORD" , city_state: "Chicago, IL" )
+# Airport.create(code: "DEN" , city_state: "Denver, CO" )
+# Airport.create(code: "JFK" , city_state: "New York, New York" )
+# Airport.create(code: "IAD" , city_state: "Washington, D.C.")
+# Airport.create(code: "SFO" , city_state: "San Francisco, CA" )
+# Airport.create(code: "AUS" , city_state: "Austin, TX")
+# Airport.create(code: "OMA" , city_state: "Omaha, NE" )
+# Airport.create(code: "SEA" , city_state: "Seattle, WA")
+
+airports = [
+    ["JIA", "Jacksonville, FL"],
+    ["MIA", "Miami, FL"],
+    ["ORD", "Chicago, IL"],
+    ["DEN", "Denver, CO"],
+    ["JFK", "New York, New York"],
+    ["IAD", "Washington, D.C."],
+    ["SFO", "San Fransisco, CA"],
+    ["AUS", "Austin, TX"],
+    ["OMA", "Omaha, NE"],
+    ["SEA", "Seattle, WA"]
+]
+
+    airports.each do |code, city_state|
+        Airport.create(code: code, city_state: city_state)
+    end
