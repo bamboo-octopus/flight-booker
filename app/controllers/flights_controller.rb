e1a039c4a114e417dd7dd645eec2a0aaa5ffe76b
bamboo_octopus <bamboooctopus@gmail.com>
2022-03-06 16:43:39 -0500

    search for flights query works, I think

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,9 +2,15 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
 
+    
+
     @departure_airport_options = Airport.all.map {|a| [a.code, a.id] }
     @arrival_airport_options = Airport.all.map {|a| [a.code, a.id] }
-    @date_options = Flight.all.map {|d| d.flight_departure_time_formatted }   
+    @date_options = Flight.all.map {|d| [d.flight_departure_time_formatted, d.flight_departure_time] } 
+
+    if params.has_key?(:departure_airport)
+      @available_flights = Flight.where(departure_airport_id: params[:departure_airport]).where(arrival_airport_id: params[:arrival_airport]).where(flight_departure_time: params[:departure_date])
+    end
 
   end
 
