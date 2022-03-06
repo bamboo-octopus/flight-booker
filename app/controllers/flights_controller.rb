f72dc22df7e3a601a0a4a0f53ae572482aa7cd84
bamboo_octopus <bamboooctopus@gmail.com>
2022-03-06 15:25:09 -0500

    added flights#index to routes

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,11 +4,14 @@ class FlightsController < ApplicationController
 
     @departure_airport_options = Airport.all.map {|a| [a.code, a.id] }
     @arrival_airport_options = Airport.all.map {|a| [a.code, a.id] }
-    @date_options = Flight.all.map {|d| d.flight_departure_time_formatted }
-
-    
-
-    
+    @date_options = Flight.all.map {|d| d.flight_departure_time_formatted }   
 
   end
+
+  private 
+  def flight_params
+    params.require(:flight).permit(:departure_airport, :arrival_airport, :departure_date, :number_of_passengers)
+  end
+
+
 end
